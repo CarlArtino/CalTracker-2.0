c5b8cbf8dc7aa3f87f097d3ed702196021437f29
AlexHelmick <47952918+AlexHelmick@users.noreply.github.com>
2021-04-27 18:43:49 -0400

    Rename file to insert.php and clean it up

--- CalTracker.php
+++ CalTracker.php
@@ -11,22 +11,9 @@
     </head>
     <body>
     <?php 
-		
-		require_once 'process.php'; 
+		require_once 'insert.php'; 
 		require_once 'delete.php'; 
-	 ?>
-
-    <?php
-
-    if (isset($_SESSION['message'])): ?>
-
-    <div class="alert alert-<?=$_SESSION['msg_type']?>">
-        <?php
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
-        ?>
-    </div>
-    <?php endif ?>
+	?>
 
 	<nav class="navbar sticky-top navbar-expand-lg navbar-light" style="background-color: #ffcfc2;">
 		<a class="navbar-brand" href="#">
@@ -57,7 +44,7 @@
 		?>
 		<div class="row justify-content-center">
 			<br><br>
-			<form action="process.php" method="POST">
+			<form action="insert.php" method="POST">
 				<input type="hidden" name="id" value="<?php echo $id; ?>">
 
 				<div class="form-row">
@@ -67,7 +54,7 @@
 							if ($update == false):
 							?>
 							<label>Name</label> <br>
-							<input type="text" name="fName" class="form-control" placeholder="Enter name">
+							<input type="text" name="name" class="form-control" placeholder="Enter name">
 							<?php endif; ?>
 						</div>
 					</div>
@@ -78,7 +65,7 @@
 							if ($update == false):
 							?>
 							<label for="exampleFormControlSelect1">Type</label>
-							<select class="form-control" name = "fType" id="exampleFormControlSelect1">
+							<select class="form-control" name = "type" id="exampleFormControlSelect1">
 							<option value="Fruit">Fruit</option>
 							<option value="Vegetable">Vegetable</option>
 							<option value="Meat">Meat</option>
@@ -95,7 +82,7 @@
 
 				<div class="form-group">
 					<label>Brand</label> <br>
-					<input type="text" name="fBrand" class="form-control" placeholder="Enter brand">
+					<input type="text" name="brand" class="form-control" placeholder="Enter brand">
 				</div>
 
 				<div class="form-row">
@@ -105,7 +92,7 @@
 							if ($update == false):
 							?>
 							<label>Calories</label> <br>
-							<input type="text" name="fCalories" class="form-control" placeholder="Enter calories">
+							<input type="text" name="calories" class="form-control" placeholder="Enter calories">
 							<?php endif; ?>
 						</div>
 					</div>
@@ -116,7 +103,7 @@
 							if ($update == false):
 							?>
 							<label>Fat (g)</label> <br>
-							<input type="text" name="fFat" class="form-control" placeholder="Enter fat">
+							<input type="text" name="fat" class="form-control" placeholder="Enter fat">
 							<?php endif; ?>
 						</div>
 					</div>
@@ -129,7 +116,7 @@
 							if ($update == false):
 							?>
 							<label>Cholesterol (mg)</label> <br>
-							<input type="text" name="fCholesterol" class="form-control" placeholder="Enter cholesterol">
+							<input type="text" name="cholesterol" class="form-control" placeholder="Enter cholesterol">
 							<?php endif; ?>
 						</div>
 					</div>
@@ -140,7 +127,7 @@
 							if ($update == false):
 							?>
 							<label>Sodium (mg)</label> <br>
-							<input type="text" name="fSodium" class="form-control" placeholder="Enter sodium">
+							<input type="text" name="sodium" class="form-control" placeholder="Enter sodium">
 							<?php endif; ?>
 						</div>
 					</div>
@@ -153,7 +140,7 @@
 							if ($update == false):
 							?>
 							<label>Carbs (g)</label> <br>
-							<input type="text" name="fCarbs" class="form-control" placeholder="Enter carbs">
+							<input type="text" name="carbs" class="form-control" placeholder="Enter carbs">
 							<?php endif; ?>
 						</div>
 					</div>
@@ -164,7 +151,7 @@
 							if ($update == false):
 							?>
 							<label>Protein (g)</label> <br>
-							<input type="text" name="fProtein" class="form-control" placeholder="Enter protein">
+							<input type="text" name="protein" class="form-control" placeholder="Enter protein">
 							<?php endif; ?>
 						</div>
 					</div>
---
+++ insert.php
@@ -0,0 +1,30 @@
+<?php
+
+if(!isset($_SESSION)){
+ session_start();
+}
+
+$mysqli = new mysqli("SG-CalTracker-4216-mysql-master.servers.mongodirector.com", "AHelmick", "FunPassword1!", "CalTracker", 3306) or die(mysqli_error(mysqli));
+
+$id = 0;
+$update = false;
+
+if (isset($_POST['save'])) {
+
+    $foodName = $_POST['name'];
+    $foodType = $_POST['type'];
+    $foodBrand = $_POST['brand'];
+    $calories = $_POST['calories'];
+    $fat = $_POST['fat'];
+	$cholesterol = $_POST['cholesterol'];
+	$sodium = $_POST['sodium'];
+	$carbs = $_POST['carbs'];
+	$protein = $_POST['protein'];
+
+    $mysqli->query("INSERT INTO foods (foodName, foodType, foodBrand, calories, fat, cholesterol, sodium, carbs, protein) 
+					VALUES(\"$foodName\", \"$foodType\", \"$foodBrand\", \"$calories\", \"$fat\", \"$cholesterol\", \"$sodium\", \"$carbs\", \"$protein\")") or die($mysqli->error);
+
+    header("location: CalTracker.php");
+}
+
+?>
