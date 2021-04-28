f6235acd323e188e34abd10c69bea46addc1a139
AlexHelmick <47952918+AlexHelmick@users.noreply.github.com>
2021-04-27 21:03:50 -0400

    Clean up mealmaker

--- MealMaker.php
+++ MealMaker.php
@@ -1,10 +1,8 @@
 <?php session_start();?>
 
 <!doctype html>
-<html lang="en">
     <head>
         <title>Meal Maker</title>
-        <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -14,32 +12,23 @@
 
 	<body>
 		<?php
-			// Database Connection
-			
+						
 			$mysqli = new mysqli("localhost", "id16688663_ahelmick", "~)qh]P#6X0B!#lg)", "id16688663_caltracker", 3306) or die(mysqli_error(mysqli));
-			
-            // Test isset for session array. set if not
 
 		    $setArray = isset($_SESSION["currentMeal"]);
 		    if (!$setArray) $_SESSION["currentMeal"] = array();
 
-		    // Test if session variable from caltracker isset. append and null if so.
-
-		    $adding = isset($_POST["ateFood"]);
+		    $adding = isset($_POST["addFood"]);
 		    if ($adding) {
-		    	array_push($_SESSION["currentMeal"], $_POST["ateFood"]);
+		    	array_push($_SESSION["currentMeal"], $_POST["addFood"]);
 		    }
 
-			// Remove an entry from the table
 			if (isset($_POST["removeFood"]))
 			{
 				unset($_SESSION["currentMeal"][$_POST["removeFood"]]);
-
-				//This reorders the array so there will not be empty indicies
 				$_SESSION["currentMeal"] = array_values($_SESSION["currentMeal"]);
 			}
 
-			// Clear entries from table if user clicked button
 			$setClear = isset($_POST["clearTable"]);
 			if($setClear)
 			{
@@ -48,79 +37,71 @@
 
 		?>
 
-	<nav class="navbar sticky-top navbar-expand-lg navbar-light" style="background-color: #ffcfc2;">
-		<a class="navbar-brand" href="#">
-			<img src="pictures/logo.png" alt="logo">
-		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav ml-md-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="MealMaker.php">Home</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
-
-	<span style="padding-left:20px">
-    <div class="container">
-
-    	<h2> Your Current Meal </h2>
-		<center>
-			<script src="javascript/hideColumns.js"></script>
-
-			<div id="checkbox_div">
-			<input type="checkbox" value="hide" id="hideExtra" onchange="hide_show_table();">Hide Extra Columns
+		<nav class="navbar sticky-top navbar-expand-lg navbar-light" style="background-color: #ffcfc2;">
+			<a class="navbar-brand" href="#">
+				<img src="pictures/logo.png" alt="logo">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ml-md-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="MealMaker.php">Home</a>
+					</li>
+				</ul>
 			</div>
-		</center>
-        <div class="row justify-content-center">
-            <table class="table">
-                    <tr>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Brand</th>
-                        <th>Calories</th>
-                        <th>Fat</th>
-						<th id="fat_col_head">Fat (g)</th>
-						<th id="cholesterol_col_head">Cholesterol (mg)</th>
-						<th id="sodium_col_head">Sodium (mg)</th>
-						<th id="carbs_col_head">Carbs (g)</th>
-						<th id="protein_col_head">Protein (g)</th>
-						<th colspan="2"></th>
-                    </tr>
+		</nav>
 
-				<form action="MealMaker.php" method="post" id="removeFood"></form>
+		<div class="container">
+		
+			<p class="h1"> Your Current Meal </p>
+			<center>
+				<script src="javascript/hideColumns.js"></script>
+				<div id="checkbox_div">
+				<input type="checkbox" value="hide" id="hideExtra" onchange="hide_show_table();">Hide Extra Columns
+				</div>
+			</center>
+			<div class="row justify-content-center">
+				<table class="table">
+						<tr>
+							<th>Name</th>
+							<th>Type</th>
+							<th>Brand</th>
+							<th>Calories</th>
+							<th id="fat_col_head">Fat (g)</th>
+							<th id="cholesterol_col_head">Cholesterol (mg)</th>
+							<th id="sodium_col_head">Sodium (mg)</th>
+							<th id="carbs_col_head">Carbs (g)</th>
+							<th id="protein_col_head">Protein (g)</th>
+							<th colspan="2"></th>
+						</tr>
 
-				<?php
-				    // Add code to print list under here
+					<form action="MealMaker.php" method="post" id="removeFood"></form>
 
-				    if ($setArray || $adding) // Tests if it has a list to display.
-				    {
-				    	$totCal = 0;   // init total calories
-		                $totFat = 0;   // init total fat
-		                $totChol = 0;  // init total cholesterol
-		                $totSod = 0;   // init total sodium
-		                $totCarb = 0;  // init total carbs
-		                $totProt = 0;  // init total protein
+					<?php
+						$totCal = 0;
+						$totFat = 0;
+						$totChol = 0;
+						$totSod = 0;
+						$totCarb = 0;
+						$totProt = 0;
 
-				    	for ($i=0; $i<count($_SESSION["currentMeal"]); $i++){
+						for ($i=0; $i<count($_SESSION["currentMeal"]); $i++){
 
-				    		$mealIndex = $_SESSION["currentMeal"][$i];
-				    		$result = $mysqli->query("SELECT * FROM foods WHERE foodId=$mealIndex") or die($mysqli_error->error); // must get specific row every time
-				    		$row = mysqli_fetch_array($result);
+							$entry = $_SESSION["currentMeal"][$i];
+							$result = $mysqli->query("SELECT * FROM foods WHERE foodId=$entry") or die($mysqli_error->error);
+							$row = mysqli_fetch_array($result);
 
-				    		// Increment totals
-				    		$totCal += $row['calories'];
-				    		$totFat += $row['fat'];
-				    		$totChol += $row['cholesterol'];
-				    		$totSod += $row['sodium'];
-				    		$totCarb += $row['carbs'];
-				    		$totProt += $row['protein'];
-				    		?>
+							$totCal += $row['calories'];
+							$totFat += $row['fat'];
+							$totChol += $row['cholesterol'];
+							$totSod += $row['sodium'];
+							$totCarb += $row['carbs'];
+							$totProt += $row['protein'];
+							?>
 
-				    		<tr>
+							<tr>
 								<td><?php echo $row['foodName'] ?></td>
 								<td><?php echo $row['foodType'] ?></td>
 								<td><?php echo $row['foodBrand'] ?></td>
@@ -135,9 +116,10 @@
 										class="btn btn-info">Remove</button>
 								</td>
 							</tr>
-							<?php
-				    	} ?>
-				    	<tr>
+						<?php	
+						}
+						?>
+						<tr>
 							<td>Total</td>
 							<td></td>
 							<td></td>
@@ -147,22 +129,19 @@
 							<td class="sodium_col"><?php echo $totSod ?></td>
 							<td class="carbs_col"><?php echo $totCarb ?></td>
 							<td class="protein_col"><?php echo $totProt ?></td>
+							<td></td>
 						</tr>
-				    	<?php
-				    }
-				?>
-            </table>
-        </div>
-        <form action="CalTracker.php" method="post" id="addFood"></form>
+				</table>
+			</div>
+			<form action="CalTracker.php" method="post" id="addFood"></form>
 
-		<button type="submit" class="btn btn-success" form="addFood" value="Submit">Add another food</button>
+			<button type="submit" class="btn btn-success" form="addFood" value="Submit">Add another food</button>
 
-		<div align="right">
-			<form onsubmit="return confirm('Do you really want to clear the table?');" action="MealMaker.php" method="post" id="clearTable"></form>
+			<div align="right">
+				<form onsubmit="return confirm('Are you sure you want to clear the table?');" action="MealMaker.php" method="post" id="clearTable"></form>
 
-			<button type="submit" form="clearTable" name="clearTable" value="True" class="btn btn-danger">Clear the table</button>
+				<button type="submit" form="clearTable" name="clearTable" value="True" class="btn btn-danger">Clear the table</button>
+			</div>
 		</div>
-    </div>
 	</body>
-
 </html>
